b894077a3d42644b3d2c59bda9b72eec6e3e2f24
chaima hannachi <>
2021-11-02 17:02:34 +0100

    second commit

--- gestion-etudiant.js
+++ gestion-etudiant.js
@@ -12,9 +12,36 @@ console.log('bonjour2');
 };
 console.log('teeeeest');
 */
+
+ // verif mail
+ var table= document.getElementsByTagName ('table')[0];//
+function verifmail(mail) {
+    var nbrows=table.rows.length;
+    console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    for (let index = 0; index < nbrows; index++) {
+        var email_list= table.getElementsByTagName ('tr')[index].cells[3].innerHTML;
+        if (email_list ==mail) {
+            return false;
+        }
+    }
+}
+//
+function verif() {
+    var mail= document.querySelector('#exampleInputEmail1').value;
+    if (verifmail(mail) ==false) {
+        $('#erreur').show();//  show pour afficher
+        $('#erreur').html('le mail existe déja');
+    }else {
+        ajout();
+        $('#erreur').hide();// hide pour caché
+        console.log('ajout')
+    }
+}
+
+//ajout d'une ligne
+var idd= 0;
 console.log('aaa');
- function click(event) {
-     event.preventDefault();
+ function ajout() {
      console.log("heeeeey");
      var prenom= document.querySelector('#prenom').value;
      var nom= document.querySelector('#nom').value;
@@ -22,22 +49,34 @@ console.log('aaa');
   
      var table= document.getElementsByTagName ('table')[0];//
      var newrow= table.insertRow(table.rows.length);//nv ligne
-
-     var cel1= newrow.insertCell(0);//les célulles
+//les célulles
+     var cel1= newrow.insertCell(0);
      var cel2= newrow.insertCell(1);
      var cel3= newrow.insertCell(2);
      var cel4= newrow.insertCell(3);
      var cel5= newrow.insertCell(4);
      var cel6= newrow.insertCell(5);
-     
-     var idd= 0;
-     //mon affichege dans les célulles 
-     cel1.innerHTML= idd+1;
+//mon affichege dans les célulles 
+var compteur=table.rows.length-1;
+     cel1.innerHTML= compteur;
      cel2.innerHTML= prenom;
      cel3.innerHTML= nom;
      cel4.innerHTML= mail;
-     //cel5.innerHTML= <FontAwesomeIcon  icon={edit}></FontAwesomeIcon>;
-     //cel6.innerHTML= <FontAwesomeIcon  icon={trash}></FontAwesomeIcon>;
+     cel5.innerHTML= '<i class="bi bi-pencil-square" id="'+compteur+'"onclick="modif(this);"></i>';//icone bootsrap
+     cel6.innerHTML= '<i class="bi bi-trash"id="'+compteur+'"onclick="supprimer(this);"></i>';
+//vider les input apres l'ajout je lui donne un evaleur vide
+   $('input').val('');
  }
+function modif(params) {
+    console.log(params.id);
+    var table= $('.table tr')[params.id];//id compteur dans <i>
+    $('#prenom').val($(table.cells[1]).text());
+    $('#nom').val($(table.cells[2]).text());
+    $('#exampleInputEmail1').val($(table.cells[3]).text());
+    $(params).closest('tr').remove();
 
 
+}
+function supprimer(params) {
+    $(params).closest('tr').remove();
+}
